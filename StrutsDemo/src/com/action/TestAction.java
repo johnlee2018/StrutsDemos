4f6d0cc3a7d2fb72fcb0b6a76bbb7d70115c93a4
lijianchun <1069815305@qq.com>
2017-02-22 21:57:35 +0800

    first struts2 demo

---
+++ StrutsDemo/src/com/action/TestAction.java
@@ -0,0 +1,22 @@
+package com.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class TestAction extends ActionSupport{
+	
+private static final long serialVersionUID=1L;
+private String helo;
+
+public String getHelo() {
+	return helo;
+}
+
+public void setHelo(String helo) {
+	this.helo = helo;
+}
+@Override
+	public String execute() throws Exception {
+		helo="hello,world";
+		return SUCCESS;
+	}
+}
